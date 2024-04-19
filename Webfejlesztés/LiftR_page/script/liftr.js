dc697b3bf08f48cfa244533c439889cb9f7535f4
J3NZ0L <jeneyzoltan2@gmail.com>
2024-04-19 13:31:26 +0200

    Majdnem jo a triangle js-el
    annyit kell kikuszobolni hogy az elso megnyitasnal is jo legyen resize nelkul

---
+++ Webfejlesztés/LiftR_page/script/liftr.js
@@ -0,0 +1,11 @@
+window.addEventListener('resize', adjustTriangle);
+
+        function adjustTriangle() {
+            var scrollbarWidth = window.innerWidth - document.documentElement.clientWidth;
+            var triangle = document.getElementById('triangle');
+            triangle.style.borderLeft = `calc(50vw - ${scrollbarWidth / 2}px) solid transparent`;
+            triangle.style.borderRight = `calc(50vw - ${scrollbarWidth / 2}px) solid transparent`;
+            triangle.style.borderTop = '100px solid #fff'; /* Change the color and height as per your requirement */
+        }
+
+    adjustTriangle();  // Call the function initially to set the correct size

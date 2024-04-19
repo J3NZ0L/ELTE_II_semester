9d1cdac3a0d8caa22f121a99d04caedc3cb999fc
J3NZ0L <jeneyzoltan2@gmail.com>
2024-04-19 18:48:27 +0200

    haromszog nagyjabol kesz
    nincsenek z indexen helyen meg a dolgok a haromszog korul

--- Webfejlesztés/LiftR_page/script/liftr.js
+++ Webfejlesztés/LiftR_page/script/liftr.js
@@ -1,11 +1,10 @@
+window.addEventListener('load', adjustTriangle);
 window.addEventListener('resize', adjustTriangle);
 
-        function adjustTriangle() {
-            var scrollbarWidth = window.innerWidth - document.documentElement.clientWidth;
-            var triangle = document.getElementById('triangle');
-            triangle.style.borderLeft = `calc(50vw - ${scrollbarWidth / 2}px) solid transparent`;
-            triangle.style.borderRight = `calc(50vw - ${scrollbarWidth / 2}px) solid transparent`;
-            triangle.style.borderTop = '100px solid #fff'; /* Change the color and height as per your requirement */
-        }
-
-    adjustTriangle();  // Call the function initially to set the correct size
+function adjustTriangle() {
+    var scrollbarWidth = window.innerWidth - document.documentElement.clientWidth;
+    var triangle = document.getElementById('triangle');
+    triangle.style.borderLeft = `calc(50vw - ${scrollbarWidth / 2}px) solid transparent`;
+    triangle.style.borderRight = `calc(50vw - ${scrollbarWidth / 2}px) solid transparent`;
+    triangle.style.borderTop = '100px solid #fff'; /* Change the color and height as per your requirement */
+}

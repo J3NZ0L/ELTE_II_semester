465510e3455ae9b4821222fcf96b220ce251642c
J3NZ0L <jeneyzoltan2@gmail.com>
2024-04-20 09:03:13 +0200

    navbar egesz jo
    lehetne reszponzivan ugyanolyan szeles mint a benne szereplo elemek

--- Webfejlesztés/LiftR_page/script/liftr.js
+++ Webfejlesztés/LiftR_page/script/liftr.js
@@ -4,7 +4,7 @@ window.addEventListener('resize', adjustTriangle);
 function adjustTriangle() {
     var scrollbarWidth = window.innerWidth - document.documentElement.clientWidth;
     var triangle = document.getElementById('triangle');
-    triangle.style.borderLeft = `calc(50vw - ${scrollbarWidth / 2}px) solid transparent`;
-    triangle.style.borderRight = `calc(50vw - ${scrollbarWidth / 2}px) solid transparent`;
-    triangle.style.borderTop = '100px solid #fff'; /* Change the color and height as per your requirement */
+    triangle.style.borderLeft = `calc(50vw - ${scrollbarWidth / 2}px) solid transparent`; //haromszog bal oldala
+    triangle.style.borderRight = `calc(50vw - ${scrollbarWidth / 2}px) solid transparent`; //haromszog jobb oldala
+    triangle.style.borderTop = '13vh solid #fff'; 
 }

5eaf26fc7aeae602a46680b197a03933b1da376e
J3NZ0L <jeneyzoltan2@gmail.com>
2024-04-23 18:00:22 +0200

    Playerig megvan bovitettbol

---
+++ Prognyelvek (Java)/Beadando/walking/game/WalkingBoardWithPlayers.java
@@ -0,0 +1,25 @@
+package walking.game;
+
+import walking.game.player.Player;
+
+public class WalkingBoardWithPlayers extends WalkingBoard{
+    private Player[] players;
+    private int round;
+    public static final int SCORE_EACH_STEP = 13;
+
+    public WalkingBoardWithPlayers(int[][] board, int playerCount) {
+        // TODO
+    }
+
+    public WalkingBoardWithPlayers(int size, int playerCount) {
+        // TODO
+    }
+
+    private void initPlayers(int playerCount) {
+        // TODO
+    }
+
+    public int[] walk(int... stepCounts) {
+        // TODO
+    }
+}
---
+++ Prognyelvek (Java)/Beadando/walking/game/player/Player.java
@@ -0,0 +1,30 @@
+package walking.game.player;
+
+import walking.game.util.Direction;
+
+public class Player{
+    private int score;
+    protected Direction direction = Direction.UP;
+
+    public int getScore() {
+        // TODO
+        return this.score;
+    }
+
+    public Direction getDirection() {
+        // TODO
+        return this.direction;
+    }
+
+    public Player() {
+        // TODO
+    }
+
+    public void addToScore(int score) {
+        // TODO
+    }
+
+    public void turn() {
+        // TODO
+    }
+}
